c0640aef479b8e515f70d58579621c0939f8ea94
LeeByoungWook <lbw3973@naver.com>
2023-03-05 20:11:44 +0900

    feat: 드래그앤 드롭의 상세기능 추가
    - 드래그앤 드롭 실행 시, 목록 순서 저장

--- js/main.js
+++ js/main.js
@@ -1,60 +1,70 @@
-import sortable from 'sortablejs';
-import {readTodos, createTodo, updateTodo, deleteTodo} from './request.js';
+import sortable from "sortablejs";
+import {
+  readTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+  reOrderTodo,
+} from "./request.js";
 
-let preventDoubleClick = false
-const buttonEl = document.querySelector('.create button')
-const inputEl = document.querySelector('.create input')
-const listContainerEl = document.querySelector('.list-container')
-const listEl = document.querySelector('.list')
-const btnAllDeleteEl = document.querySelector('.btn-all-delete')
-const modalContainerEl = document.querySelector('.modal-container')
-const inputModifyEl = document.querySelector('#input-modify')
-const btnModifyEl = document.querySelector('#btn-modify')
-const btnCancelEl = document.querySelector('#btn-cancel')
-const fixedLoadingEl = document.querySelector('.fixed-loading')
+let preventDoubleClick = false;
+const buttonEl = document.querySelector(".create button");
+const inputEl = document.querySelector(".create input");
+const listContainerEl = document.querySelector(".list-container");
+const listEl = document.querySelector(".list");
+const btnAllDeleteEl = document.querySelector(".btn-all-delete");
+const modalContainerEl = document.querySelector(".modal-container");
+const inputModifyEl = document.querySelector("#input-modify");
+const btnModifyEl = document.querySelector("#btn-modify");
+const btnCancelEl = document.querySelector("#btn-cancel");
+const fixedLoadingEl = document.querySelector(".fixed-loading");
 
-let inputText = ''
-inputEl.addEventListener('input', () => {
-  inputText = inputEl.value
-})
+let inputText = "";
+inputEl.addEventListener("input", () => {
+  inputText = inputEl.value;
+});
 
-inputEl.addEventListener('keyup', e => {
-  if(e.key === 'Enter'){
-    buttonEl.click()
-    inputEl.value = ''
+inputEl.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    buttonEl.click();
+    inputEl.value = "";
   }
-})
+});
 
-buttonEl.addEventListener('click', async () => {
-  if(preventDoubleClick) {return}
-  preventDoubleClick = true
-  fixedLoadingEl.style.display = 'block'
-  await createTodo(inputText)
-  const todos = await readTodos()
-  renderTodos(todos)
-  preventDoubleClick = false
-})
+buttonEl.addEventListener("click", async () => {
+  if (preventDoubleClick) {
+    return;
+  }
+  preventDoubleClick = true;
+  fixedLoadingEl.style.display = "block";
+  const liEls = Array.from(document.querySelectorAll(".list li"));
+  await createTodo(inputText, liEls.length);
+  const todos = await readTodos();
+  renderTodos(todos);
+  preventDoubleClick = false;
+});
 
 new sortable(listEl, {
-  handle: '.handle',
+  handle: ".handle",
   animation: 150,
 });
 
-;(async () => {
-  const todos = await readTodos()
-  renderTodos(todos)
-})()
+(async () => {
+  modalContainerEl.style.display = "none";
+  const todos = await readTodos();
+  renderTodos(todos);
+})();
 
-const renderTodos = async todos => {
-  let index = 0
-  const liEls = todos.map(todo => {
+const renderTodos = async (todos) => {
+  let index = 0;
+  const liEls = todos.map((todo) => {
+    const createdAt = new Date(todo.createdAt).toLocaleString();
+    const updatedAt = new Date(todo.updatedAt).toLocaleString();
 
-    const createdAt = new Date(todo.createdAt).toLocaleString()
-    const updatedAt = new Date(todo.updatedAt).toLocaleString()
-
-    const liEl = document.createElement('li')
-    liEl.dataset.index = index++
-    liEl.innerHTML = /* html */`
+    const liEl = document.createElement("li");
+    liEl.dataset.id = todo.id;
+    liEl.dataset.index = index++;
+    liEl.innerHTML = /* html */ `
     <div class="material-symbols-outlined handle">apps</div>
     <input type="checkbox">
     <p class="title">${todo.title}</p>
@@ -62,101 +72,123 @@ const renderTodos = async todos => {
       <span>작성: ${createdAt}</span>
       <span>수정: ${updatedAt}</span>
     </div>
-     `
-    
-    const checkEl = liEl.querySelector('input')
-    checkEl.checked = todo.done
-    checkEl.addEventListener('click', async () => {
-      todo.done = checkEl.checked
-      await updateTodo(todo)
-      const todos = await readTodos()
-      renderTodos(todos)
-    })
+     `;
+
+    const checkEl = liEl.querySelector("input");
+    checkEl.checked = todo.done;
+    checkEl.addEventListener("click", async () => {
+      todo.done = checkEl.checked;
+      await updateTodo(todo);
+      const todos = await readTodos();
+      renderTodos(todos);
+    });
 
     // Edit modal show
-    const pEl = liEl.querySelector('p')
-    pEl.addEventListener('dblclick', (e) => {
-      makemodal(pEl)
-    })
+    const pEl = liEl.querySelector("p");
+    pEl.addEventListener("dblclick", (e) => {
+      makemodal(pEl);
+    });
 
-    const btnEl = document.createElement('button')
-    btnEl.classList.add('btn-delete')
-    btnEl.addEventListener('click', async () => {
-      if(!confirm("삭제 하시겠습니까?")) { return } 
-      fixedLoadingEl.style.display = 'block'
+    const btnEl = document.createElement("button");
+    btnEl.classList.add("btn-delete");
+    btnEl.addEventListener("click", async () => {
+      if (!confirm("삭제 하시겠습니까?")) {
+        return;
+      }
+      fixedLoadingEl.style.display = "block";
 
-      await deleteTodo(todo)
-      const todos = await readTodos()
-      renderTodos(todos)
-    })
-    liEl.append(btnEl)
+      await deleteTodo(todo);
+      const todos = await readTodos();
+      renderTodos(todos);
+    });
+    liEl.append(btnEl);
 
-
-    if(checkEl.checked === true){
-      checkEl.parentElement.querySelector('p').style.textDecorationLine = "line-through"
+    if (checkEl.checked === true) {
+      checkEl.parentElement.querySelector("p").style.textDecorationLine =
+        "line-through";
     }
+    liEl.addEventListener("drop", async () => {
+      const liEls = Array.from(document.querySelectorAll(".list li"));
+      const res = liEls.map((li) => li.dataset.id);
+      await reOrderTodo(res);
+    });
 
-    return liEl
-  })
-  listEl.innerHTML = ''
-  listEl.append(...liEls)
+    return liEl;
+  });
+  listEl.innerHTML = "";
+  listEl.append(...liEls);
 
-  listContainerEl.style.backgroundImage = 'none'
-  fixedLoadingEl.style.display = 'none'
-  
-  listEl.childElementCount < 1 ? btnAllDeleteEl.style.display = 'none' : btnAllDeleteEl.style.display = 'block'
-}
+  listContainerEl.style.backgroundImage = "none";
+  fixedLoadingEl.style.display = "none";
+
+  listEl.childElementCount < 1
+    ? (btnAllDeleteEl.style.display = "none")
+    : (btnAllDeleteEl.style.display = "block");
+};
 
 function makemodal(El) {
-  const inputEl = modalContainerEl.querySelector('input')
-  inputEl.value = El.textContent
-  btnModifyEl.dataset.index = El.parentElement.dataset.index
+  const inputEl = modalContainerEl.querySelector("input");
+  inputEl.value = El.textContent;
+  btnModifyEl.dataset.id = El.parentElement.dataset.id;
 
-  modalContainerEl.style.display = 'block'
+  modalContainerEl.style.display = "block";
+  inputEl.focus();
 }
 
-window.addEventListener('keyup', e => {
-  if(e.key === 'Escape'){
-    if(!confirm("취소 하시겠습니까?")) { return }
-    modalContainerEl.style.display = 'none'
+window.addEventListener("keyup", (e) => {
+  if (modalContainerEl.style.display == "none") {
+    return;
   }
-})
-
-    
-btnModifyEl.addEventListener('click', async () => {
-  if(!confirm("수정 하시겠습니까?")) { return }
-
-  let todo = await readTodos()
-  todo = todo[btnModifyEl.dataset.index]  
-  todo.title = inputModifyEl.value
-  await updateTodo(todo)
-
-  const todos = await readTodos()
-  renderTodos(todos)
-  modalContainerEl.style.display = 'none'
-})
-
-btnCancelEl.addEventListener('click', () => {
-  if(!confirm("취소 하시겠습니까?")) { return }
-
-  modalContainerEl.style.display = 'none'
-})
-
-inputModifyEl.addEventListener('keyup', e => {
-  if(e.key === 'Enter'){
-    btnModifyEl.click()
+  if (e.key === "Escape") {
+    if (!confirm("취소 하시겠습니까?")) {
+      return;
+    }
+    modalContainerEl.style.display = "none";
   }
-})
+});
 
-btnAllDeleteEl.addEventListener('click', async () => {
-  if (!confirm("전체 삭제 하시겠습니까?")) { return; }
+btnModifyEl.addEventListener("click", async (e) => {
+  if (!confirm("수정 하시겠습니까?")) {
+    return;
+  }
 
-  fixedLoadingEl.style.display = 'block'
-  
+  const todo = (await readTodos()).find(
+    (todo) => todo.id == e.target.dataset.id
+  );
+  todo.id = e.target.dataset.id;
+  todo.title = inputModifyEl.value;
+  await updateTodo(todo);
+
+  const todos = await readTodos();
+  renderTodos(todos);
+  modalContainerEl.style.display = "none";
+});
+
+btnCancelEl.addEventListener("click", () => {
+  if (!confirm("취소 하시겠습니까?")) {
+    return;
+  }
+
+  modalContainerEl.style.display = "none";
+});
+
+inputModifyEl.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    btnModifyEl.click();
+  }
+});
+
+btnAllDeleteEl.addEventListener("click", async () => {
+  if (!confirm("전체 삭제 하시겠습니까?")) {
+    return;
+  }
+
+  fixedLoadingEl.style.display = "block";
+
   const todos = await readTodos();
   await todos.map((todo) => {
     deleteTodo(todo);
   });
 
   renderTodos([]);
-})
+});
